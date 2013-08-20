9bc96d933e656f51a44ac5c327dd24a86c70104b
Nicholas Plante <votus998@gmail.com>
2013-08-20 08:49:33 -0400

    Remerge

--- src/com/github/triarry/iRestore/iRestorePlayerListener.java
+++ src/com/github/triarry/iRestore/iRestorePlayerListener.java
@@ -66,7 +66,9 @@ public class iRestorePlayerListener implements Listener {
             		MyPet myPet = ((CraftMyPet) a.getShooter()).getMyPet();
             		killer = myPet.getOwner().getName() + "'s pet " + myPet.getPetName();
             	}
-            	else 
+            	else if(a.getShooter() instanceof Player) 
+            		killer = a.getShooter().getCustomName();
+            	else
             		return;
             }
             else if(iRestore.myPetEnabled && lastDamageEvent.getDamager() instanceof CraftMyPet && plugin.getConfig().getBoolean("my-pet-enabled") == true) {
